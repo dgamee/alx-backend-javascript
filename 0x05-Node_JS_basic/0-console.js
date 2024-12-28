7807aa47e391be39a11dfebab63dc1a819b80dec
rayden <rayden.ai@gmail.com>
2024-12-28 15:28:02 +0100

    catch up

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+* Function to print a message in the console
+* @param {string} message - Message to print
+*/
+
+function displayMessage(message) {
+  console.log(message); // Print the message to the console
+}
+
+// Export function to make it available to other modules
+module.exports = displayMessage;
